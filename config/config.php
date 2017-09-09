0d5df0e39de96c16903aaf7d2ac54601beb2ca0f
Oliwier Thomas <minecraft.thomas234@gmail.com>
2017-09-09 23:42:03 +0200

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,29 @@
+<?php
+/*
+    Author: toster234
+*/
+
+
+/************* General configuration *************/
+$config["general"]["title"]         = "CSGO-24.pl";                                 				// Website title - displayed in the footer
+$config["general"]["text1"]         = "This site is coming soon!";                  				// Title - displayed in green box
+$config["general"]["text2"]         = "The site is under construction. The site will be open soon :)";		// Description - displayed in green box
+
+
+
+/************* Contact panel configuration *************/
+
+$config['contact']['title'] = 'Contact the staff';
+
+/*
+TIP: You can remove all items below to hide contact panel
+
+CONTACT PANEL SYNTAX:
+$config['contact']['items'][] = ["name", "link description", "link"];
+
+FOR EXAMPLE:
+$config['contact']['items'][] = ["Steam", "toster234", "https://steamcommunity.com/id/toster234"];
+*/
+
+$config['contact']['items'][] = ["Email", "support@csgo-24.pl", "mailto:support@csgo-24.pl"];
+?>
