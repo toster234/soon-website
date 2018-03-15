1dfc3811edc42e99b171fdab869a421d9edfe163
Oliwier <minecraft.thomas234@gmail.com>
2018-03-15 19:39:00 +0100

    Update config.php
    Small change.

--- config/config.php
+++ config/config.php
@@ -5,7 +5,7 @@
 
 
 /************* General configuration *************/
-$config["general"]["title"]         = "CSGO-24.pl";                                 				// Website title - displayed in the footer
+$config["general"]["title"]         = "Website title";                                 				// Website title - displayed in the footer
 $config["general"]["text1"]         = "This site is coming soon!";                  				// Title - displayed in green box
 $config["general"]["text2"]         = "The site is under construction. The site will be open soon :)";		// Description - displayed in green box
 
@@ -25,5 +25,5 @@ FOR EXAMPLE:
 $config['contact']['items'][] = ["Steam", "toster234", "https://steamcommunity.com/id/toster234"];
 */
 
-$config['contact']['items'][] = ["Email", "support@csgo-24.pl", "mailto:support@csgo-24.pl"];
+$config['contact']['items'][] = ["Email", "support@example.com", "mailto:support@example.com"];
 ?>
